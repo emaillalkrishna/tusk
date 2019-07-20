57590be2db418d899a5f4b7fc27399831a23cf7e
Lal Krishna <45747519+emaillalkrishna@users.noreply.github.com>
2019-07-20 21:42:50 +0530

    excel code added to the data.py file

---
+++ data.py
@@ -0,0 +1,12 @@
+# # # Step 15   -> Excel sheet is not working
+#
+# MY_CHROME_PATH = "C:/Users/LAL KRISHNA/PycharmProjects/Jenkins_POM/drivers/chromedriver.exe"
+# JENKINS_URL = "http://localhost:8080/login?from=%2F"
+# # UN = "admin"
+# # PW = "manager"
+#
+# Workbook_path =  "C:/Users/LAL KRISHNA/Documents/QSpiders Files Downloaded/MyExcelFile07July/testdata.xlrd"
+# Sheet_name = "Sheet1"
+#
+# Column_name_UN = "UN"
+# Column_name_PWD = "PWD"
